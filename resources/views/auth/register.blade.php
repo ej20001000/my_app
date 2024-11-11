b3d9260095bb5a8e0b5193260b49a92e172fd2ab
ej20001000 <ejayyj1225@gmail.com>
2024-11-11 23:09:01 +0900

    study

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,29 @@
 <x-layout>
     <h1 class="title">Register a new Account</h1>
+
+    <div class="mx-auto max-w-screen-sm card">
+        <form action="" method="POST">
+            <div class="mb-4">
+                <label for="username">Username</label>
+                <input type="text" name="username" class="input">
+            </div>
+
+            <div class="mb-4">
+                <label for="email">Email</label>
+                <input type="text" name="email" class="input">
+            </div>
+
+            <div class="mb-4">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="input">
+            </div>
+
+            <div class="mb-8">
+                <label for="password_confirmation">Confirm Password</label>
+                <input type="password" name="password_confirmation" class="input">
+            </div>
+
+            <button class="btn">Register</button>
+        </form>
+    </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'posts.index') -> name('home');
@@ -7,3 +8,5 @@ Route::view('/', 'posts.index') -> name('home');
 Route::get('/register', function () {
     return view('auth.register');
 }) -> name('register');
+
+Route::post('/register', [AuthController::class, 'register']);
